e2e3bcb17c67d37c682556de4c067491f3d21e35
Ed van Bruggen <edvb54@gmail.com>
2014-10-24 15:50:53 -0700

    Bin: Make edium.sh a hard link

--- bin/edium.sh
+++ bin/edium.sh
@@ -1,1 +1,28 @@
-/home/ed/Code/Bash/edium/edium.sh
+#!/usr/bin/env bash
+
+# ed: EDit file or folder with your EDITOR
+# hey
+
+ed() {
+	if [[ "$@" == "" ]]; then
+		$EDITOR .
+	else
+		$EDITOR $@
+	fi
+}
+
+# ED: Rotate between EDITORs
+
+ED() {
+	local EDITOR_new
+	if [[ $EDITOR == "vim" ]]; then
+		EDITOR_new="emacs"
+	elif [[ $EDITOR == "emacs" ]]; then
+		EDITOR_new="vim"
+	fi
+
+	echo "Setting EDITOR to $EDITOR_new"
+	EDITOR=$EDITOR_new
+	echo "EDITOR is now $EDITOR"
+}
+
