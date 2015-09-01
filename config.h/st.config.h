4d350080134f77ac38aadf8e942dfbce19d1c8f0
Ed van Bruggen <edvb54@gmail.com>
2015-08-31 21:55:58 -0700

    Config.h: Change copy and paste shortcuts to ctrl, not alt

--- config.h/st.config.h
+++ config.h/st.config.h
@@ -132,17 +132,17 @@ static Mousekey mshortcuts[] = {
 
 static Shortcut shortcuts[] = {
 	/* mask                 keysym          function        argument */
-	{ ControlMask,          XK_Print,       toggleprinter,  {.i =  0} },
-	{ ShiftMask,            XK_Print,       printscreen,    {.i =  0} },
-	{ XK_ANY_MOD,           XK_Print,       printsel,       {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_Prior,       xzoom,          {.f = +1} },
-	{ MODKEY|ShiftMask,     XK_Next,        xzoom,          {.f = -1} },
-	{ MODKEY|ShiftMask,     XK_Home,        xzoomreset,     {.f =  0} },
-	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_Insert,      clippaste,      {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_C,           clipcopy,       {.i =  0} },
-	{ MODKEY|ShiftMask,     XK_V,           clippaste,      {.i =  0} },
-	{ MODKEY,               XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ControlMask,           XK_Print,    toggleprinter, {.i =  0} },
+	{ ShiftMask,             XK_Print,    printscreen,   {.i =  0} },
+	{ XK_ANY_MOD,            XK_Print,    printsel,      {.i =  0} },
+	{ MODKEY|ShiftMask,      XK_Prior,    xzoom,         {.f = +1} },
+	{ MODKEY|ShiftMask,      XK_Next,     xzoom,         {.f = -1} },
+	{ MODKEY|ShiftMask,      XK_Home,     xzoomreset,    {.f =  0} },
+	{ ShiftMask,             XK_Insert,   selpaste,      {.i =  0} },
+	{ ControlMask|ShiftMask, XK_Insert,   clippaste,     {.i =  0} },
+	{ ControlMask|ShiftMask, XK_C,        clipcopy,      {.i =  0} },
+	{ MODKEY|ShiftMask,      XK_V,        clippaste,     {.i =  0} },
+	{ MODKEY,                XK_Num_Lock, numlock,       {.i =  0} },
 };
 
 /*
