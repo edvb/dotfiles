5a999b810b382129424972a9acbcf29a22dfb1ff
Ed van Bruggen <edvb54@gmail.com>
2016-04-07 22:09:25 -0700

    Config.h: Change slock to show black when clear

--- config.h/slock.config.h
+++ config.h/slock.config.h
@@ -3,4 +3,4 @@ static const char *colorname[NUMCOLS] = {
 	"#268bd2",   /* during input */
 	"#dc322f",   /* failed/cleared the input */
 };
-static const Bool failonclear = True;
+static const Bool failonclear = False;
