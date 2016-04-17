31c10b68f6397e4febe78b4148eda93a9ecea707
Ed van Bruggen <edvb54@gmail.com>
2016-04-16 22:17:55 -0700

    Config.h: Update slock config.h

--- config.h/slock.config.h
+++ config.h/slock.config.h
@@ -1,6 +1,8 @@
 static const char *colorname[NUMCOLS] = {
 	"#002b36",   /* after initialization */
 	"#268bd2",   /* during input */
-	"#dc322f",   /* failed/cleared the input */
+	"#dc322f",   /* wrong password */
 };
-static const Bool failonclear = False;
+
+/* treat a cleared input like a wrong password */
+static const int failonclear = 0;
