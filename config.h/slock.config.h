9d3a32eb875382794750caaf1cd8afacee5b1d78
Ed van Bruggen <edvb54@gmail.com>
2016-11-23 19:22:42 -0800

    Config.h: Update slock to newest version

--- config.h/slock.config.h
+++ config.h/slock.config.h
@@ -1,8 +1,12 @@
+/* user and group to drop privileges to */
+static const char *user  = "nobody";
+static const char *group = "nogroup";
+
 static const char *colorname[NUMCOLS] = {
-	"#002b36",   /* after initialization */
-	"#268bd2",   /* during input */
-	"#dc322f",   /* wrong password */
+	[INIT]   = "#002b36",   /* after initialization */
+	[INPUT]  = "#268bd2",   /* during input */
+	[FAILED] = "#dc322f",   /* wrong password */
 };
 
-/* treat a cleared input like a wrong password */
+/* treat a cleared input like a wrong password (color) */
 static const int failonclear = 0;
